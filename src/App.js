d465b0c497aacb76fa52c2a89bc1c2f84765d41f
Florian Findelsberger <florian.findelsberger@gmail.com>
2020-08-30 18:15:35 +0200

    Added Switch statement to Router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import React from 'react';
 import Chat from './components/Chat/Chat';
 import Join from './components/Join/Join';
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
   return (
+    // wenn hier nich exact Path angegeben wird bekommt man einen Error sollte man mit einer url direct auf /chat zugreifen will da der / auch bei der ersten Route gematched wird
     <Router>
-      <Route path="/" exact component={Join} />
-      <Route path="/chat" component={Chat} />
+      <Switch>
+        <Route exact path="/" component={Join} />
+        <Route path="/chat" component={Chat} />
+      </Switch>
     </Router>
   );
 }
