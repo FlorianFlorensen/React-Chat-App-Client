15efe0358c2d2f4a64f3e2e1adcf4d85670094d9
Florian Findelsberger <florian.findelsberger@gmail.com>
2020-08-30 12:25:10 +0200

    Added join Input Validation

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -22,8 +22,8 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  //const ENDPOINT = 'https://dontsnitchonmebackend.herokuapp.com';
-  const ENDPOINT = 'localhost:5000';
+  const ENDPOINT = 'https://dontsnitchonmebackend.herokuapp.com';
+  //const ENDPOINT = 'localhost:5000';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
